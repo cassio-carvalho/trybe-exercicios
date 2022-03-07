88c1fef3fa7ee236c4ff06744fa56317131d4a03
Cássio Carvalho <cassio.acarvalho@gmail.com>
2022-03-07 19:57:20 -0300

    Exercicio 3

--- Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_4/4.1/dia-1-javascript-primeiros-passos/script.js
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_4/4.1/dia-1-javascript-primeiros-passos/script.js
@@ -1,21 +1,38 @@
-// Exercício 1
-const a = 5;
-const b = 10;
+// // Exercício 1
+// const a = 5;
+// const b = 10;
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a % b);
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
 
-// Exercício 2
+// // Exercício 2
 
-const num1 = 2;
-const num2 = 3;
+// const num1 = 2;
+// const num2 = 3;
 
-if (num1 > num2) {
-  console.log(num1);
+// if (num1 > num2) {
+//   console.log("Maior é", num1);
+// }
+// else {
+//   console.log("Maior é", num2)
+// }
+
+// Exercício 3
+
+const num3 = 20;
+const num4 = 30;
+const num5 = 40;
+
+if (num3 > num4 && num3 > num5) {
+  console.log("Maior número é", num3);
+}
+else if (num4 > num3 && num4 > num5) {
+  console.log("Maior número é", num4);
 }
 else {
-  console.log(num2)
+  console.log("Maior número é", num5);
 }
+

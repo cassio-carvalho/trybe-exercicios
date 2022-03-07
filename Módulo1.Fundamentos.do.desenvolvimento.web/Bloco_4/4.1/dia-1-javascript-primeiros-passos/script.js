6c9c2902edb74cd2e199f9a25abcd6b0cf3e68a3
Cássio Carvalho <cassio.acarvalho@gmail.com>
2022-03-07 19:21:55 -0300

    Exercicio 2

--- Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_4/4.1/dia-1-javascript-primeiros-passos/script.js
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_4/4.1/dia-1-javascript-primeiros-passos/script.js
@@ -1,3 +1,4 @@
+// Exercício 1
 const a = 5;
 const b = 10;
 
@@ -7,3 +8,14 @@ console.log(a * b);
 console.log(a / b);
 console.log(a % b);
 
+// Exercício 2
+
+const num1 = 2;
+const num2 = 3;
+
+if (num1 > num2) {
+  console.log(num1);
+}
+else {
+  console.log(num2)
+}

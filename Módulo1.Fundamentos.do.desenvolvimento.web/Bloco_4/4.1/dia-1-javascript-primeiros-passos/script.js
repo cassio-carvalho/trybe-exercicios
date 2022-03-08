31181ac68da9cc3decfd35fcacd33a75f15f62f7
Cássio Carvalho <cassio.acarvalho@gmail.com>
2022-03-07 22:25:52 -0300

    Exercicio 8

--- Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_4/4.1/dia-1-javascript-primeiros-passos/script.js
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_4/4.1/dia-1-javascript-primeiros-passos/script.js
@@ -95,26 +95,39 @@
 
 // Exercício 7
 
-const nota = 10;
+// const nota = 10;
 
-if (nota > 90 && nota <= 100) {
-  console.log("Nota A");
+// if (nota > 90 && nota <= 100) {
+//   console.log("Nota A");
+// }
+// else if (nota >= 80 && nota < 90) {
+//   console.log("Nota B");
+// }
+// else if (nota >= 70 && nota < 80) {
+//   console.log("Nota C");
+// }
+// else if (nota >= 60 && nota < 70) {
+//   console.log("Nota D");
+// }
+// else if (nota >= 50 && nota < 60) {
+//   console.log("Nota E");
+// }
+// else if (nota < 50 && nota > 0) {
+//   console.log("Nota F");
+// }
+// else {
+//   console.log("Erro! Nota inválida.")
+// }
+
+// Exercício 8
+
+const a = 1;
+const b = 1;
+const c = 3;
+
+if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+  console.log(true);
 }
-else if (nota >= 80 && nota < 90) {
-  console.log("Nota B");
-}
-else if (nota >= 70 && nota < 80) {
-  console.log("Nota C");
-}
-else if (nota >= 60 && nota < 70) {
-  console.log("Nota D");
-}
-else if (nota >= 50 && nota < 60) {
-  console.log("Nota E");
-}
-else if (nota < 50 && nota > 0) {
-  console.log("Nota F");
-}
-else {
-  console.log("Erro! Nota inválida.")
+else{
+  console.log(false);
 }

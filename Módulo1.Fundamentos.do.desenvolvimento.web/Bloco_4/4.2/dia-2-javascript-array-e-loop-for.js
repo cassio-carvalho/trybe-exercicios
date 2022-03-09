a382696f503eafabda5e3c0b07422e14315af628
Cássio Carvalho <cassio.acarvalho@gmail.com>
2022-03-08 21:50:56 -0300

    Correcao Exercicio 1

--- Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_4/4.2/dia-2-javascript-array-e-loop-for.js
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_4/4.2/dia-2-javascript-array-e-loop-for.js
@@ -1,5 +1,15 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-// Exercicio 1
+ // Exercicio 1
 
-console.log(numbers);
+ for (let i = 0; i < numbers.length; i += 1) {
+  console.log(i);
+}
+
+// Exercício 2
+
+// soma = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   console.log([i]);
+// }

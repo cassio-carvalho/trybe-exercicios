49899ee70263f2eb1eda86ab8bf3fda910437e9d
Cássio Carvalho <cassio.acarvalho@gmail.com>
2022-03-10 12:46:28 -0300

    Exercicio 3

--- Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_4/4.3/dia-3-javascript-logica-de-programacao-e-algoritmos.js
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_4/4.3/dia-3-javascript-logica-de-programacao-e-algoritmos.js
@@ -10,8 +10,28 @@
 
 // Exercicio 2
 
-let word = 'banana';
-let newWord = word.split('').reverse().join('')  
-// Fonte: https://stackoverflow.com/questions/958908/how-do-you-reverse-a-string-in-place-in-javascript
+// let word = 'banana';
+// let newWord = word.split('').reverse().join('')  
+// // Fonte: https://stackoverflow.com/questions/958908/how-do-you-reverse-a-string-in-place-in-javascript
 
-console.log(newWord);
+// console.log(newWord);
+
+// Exercício 3
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = '';
+let menorPalavra = '';
+
+for (index = 0; index < array.length; index += 1) {
+  if (maiorPalavra.length < array[index].length) {
+    maiorPalavra = array[index];
+  }
+}
+for (index2 = 0; index2 < array.length; index2 += 1) {
+  menorPalavra = array[index2];
+  if (menorPalavra.length > array[index2].length) {
+    menorPalavra = array[index2];
+  }
+}
+console.log(maiorPalavra);
+console.log(menorPalavra);

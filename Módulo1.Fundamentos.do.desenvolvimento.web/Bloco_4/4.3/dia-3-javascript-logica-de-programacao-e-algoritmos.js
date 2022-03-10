d57c17802c4abc8646ba77f5498ae28394ce8397
Cássio Carvalho <cassio.acarvalho@gmail.com>
2022-03-09 22:21:46 -0300

    Inicio Exercicio 2

--- Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_4/4.3/dia-3-javascript-logica-de-programacao-e-algoritmos.js
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_4/4.3/dia-3-javascript-logica-de-programacao-e-algoritmos.js
@@ -1,9 +1,20 @@
 // Exercicio 1
 
-let numero = 10;
-let fatorial = 1;
+// let numero = 10;
+// let fatorial = 1;
 
-for (let index = numero; index > 0; index -= 1) {
-  fatorial *= index;
+// for (let index = numero; index > 0; index -= 1) {
+//   fatorial *= index;
+// }
+// console.log(fatorial);
+
+// Exercicio 2
+
+let word = 'tryber';
+let newWord = null;
+
+for (let index = word.length; index > 0; index -= 1) {
+  let char = word.slice(index);
+  newWord = ;
 }
-console.log(fatorial);
+console.log(newWord);

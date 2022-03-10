96bb831db9adb5a66bff3f2df6a27d5607b1b756
Cássio Carvalho <cassio.acarvalho@gmail.com>
2022-03-10 18:14:34 -0300

    Atualizações do arquivo

---
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_4/4.3/dia-3-javascript-logica-de-programacao-e-algoritmos.js
@@ -0,0 +1,43 @@
+// Exercicio 1
+
+// let numero = 10;
+// let fatorial = 1;
+
+// for (let index = numero; index > 0; index -= 1) {
+//   fatorial *= index;
+// }
+// console.log(fatorial);
+
+// Exercicio 2
+
+// let word = 'banana';
+// let newWord = word.split('').reverse().join('')  
+// // Fonte: https://stackoverflow.com/questions/958908/how-do-you-reverse-a-string-in-place-in-javascript
+
+// console.log(newWord);
+
+// Exercício 3
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let maiorPalavra = '';
+// let menorPalavra = '';
+
+// for (index = 0; index < array.length; index += 1) {
+//   if (maiorPalavra.length < array[index].length) {
+//     maiorPalavra = array[index];
+//   }
+// }
+// for (index2 = 0; index2 < array.length; index2 += 1) {
+//   menorPalavra = array[index2];
+//   if (menorPalavra.length > array[index2].length) {
+//     menorPalavra = array[index2];
+//   }
+// }
+// console.log(maiorPalavra);
+// console.log(menorPalavra);
+
+// Exercício 4
+
+let numeros = [];
+let primo = 0;
+

71b078f24ce979bcbd821d396b1a9b9abb740d35
Cássio Carvalho <cassio.acarvalho@gmail.com>
2022-03-10 18:14:10 -0300

    Atualizações do arquivo

---
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_4/4.2/dia-2-javascript-array-e-loop-for.js
@@ -0,0 +1,127 @@
+//  // Exercicio 1
+
+//  for (let i = 0; i < numbers.length; i += 1) {
+//   console.log(i);
+// }
+
+// Exercício 2
+
+// let soma = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     soma += numbers[i];
+//   }
+//   console.log(soma);
+
+// Exercício 3
+
+// let soma = 0;
+// let media = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     soma += numbers[i];
+//   }
+//   media = soma / numbers.length;
+//   console.log(media);
+
+// Exercício 4
+
+// let soma = 0;
+// let media = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     soma += numbers[i];
+//   }
+//   media = soma / numbers.length;
+//   if (media > 20) {
+//     console.log("Valor maior que 20");
+//   }
+//   else {
+//     console.log("Valor menor que 20");
+//   }
+
+// Exercício 5
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// let maiorNum = 0;
+// let numero = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   numero = numbers[i];
+//   maiorNum = Math.max(maiorNum, numero);
+// }
+// console.log(maiorNum);
+
+// Exercício 6
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// let impares = 0;
+// let numero = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   numero = numbers[i];
+//   if (numero % 2 !== 0) {
+//     impares += 1;
+//   }
+// }
+// if (impares > 0) {
+//   console.log(impares);
+// }
+// else {
+//   console.log("nenhum valor ímpar encontrado");
+// }
+
+// Exercício 7
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// let menorNum = numbers[0];
+// let numero = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   numero = numbers[i];
+//   menorNum = Math.min(menorNum, numero);
+// }
+// console.log(menorNum)
+
+// Exercício 8
+
+// let numeros = [];
+
+// for (let i = 1; i < 26; i += 1) {
+//   numeros.push(i);
+// }
+// console.log(numeros);
+
+// Exercício 9
+
+// let numeros = [];
+
+// for (let i = 1; i < 26; i += 1) {
+//   numeros.push(i);
+// }
+// for (let z = 0; z < numeros.length; z += 1) {
+//   console.log(numeros[z] / 2);
+// }
+
+// Bônus
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+position = null;
+
+for (let i = 1; i < numbers.length; i += 1) {
+  for (let z = 0; z < numbers.length - i; z += 1) {
+    if (numbers[i] < numbers [z]) {
+      let position = numbers[i];
+      numbers[i] = numbers[z];
+      numbers[z] = position;
+      console.log(position);
+    }
+  }
+}
+
+// for (let y = 0; y < numbers.length; y += 1) {
+//     console.log(numbers[y]);
+//   }

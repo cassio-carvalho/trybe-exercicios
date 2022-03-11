ab7ac3264f42783efe78cf11ef18d5683ebd76e8
Cássio Carvalho <cassio.acarvalho@gmail.com>
2022-03-11 12:05:24 -0300

    Exercicio 6

--- Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_4/4.4/dia-4-javascript-objetos-e-funcoes.js
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_4/4.4/dia-4-javascript-objetos-e-funcoes.js
@@ -6,9 +6,9 @@
 
 // Exercício 1
 
-  // console.log('Bem vinda, ' + info.personagem);
+// console.log('Bem vinda, ' + info.personagem);
 
- // Exercício 2
+// Exercício 2
 
 // info.recorrente = 'Sim';
 // console.log(info);
@@ -26,30 +26,56 @@
 // }
 
 // Exercício 5
-let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
+
+// info.recorrente = 'Sim';
+// console.log(info);
+
+// let info2 = {
+//   personagem: 'Tio Patinhas',
+//   origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//   nota: 'O último MacPatinhas',
+//   recorrente: 'Sim',
+// };
+
+// function imprimeAmbos (d1, d2) {
+//   for (dado in d1) {
+//     if (dado === 'recorrente' && d1[dado] === 'Sim' && d2[dado] === 'Sim'){
+//       console.log('Ambos recorrentes');
+//     }
+//     else {
+//       console.log(d1[dado] + ' e ' + d2[dado]);
+//     }
+//   }
+// }
+// imprimeAmbos (info, info2);
+
+// Exercício 6
+
+let leitor = {
+  nome: "Julia",
+  sobrenome: "Pessoa",
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: "O Pior Dia de Todos",
+      autor: "Daniela Kopsch",
+      editora: "Tordesilhas",
+    },
+  ],
 };
 
-info.recorrente = 'Sim';
-console.log(info);
+let infoBook = leitor.livrosFavoritos[0].titulo;
 
-let info2 = {
-  personagem: 'Tio Patinhas',
-  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-  nota: 'O último MacPatinhas',
-  recorrente: 'Sim',
-};
-
-function imprimeAmbos (d1, d2) {
-  for (dado in d1) {
-    if (dado === 'recorrente' && d1[dado] === 'Sim' && d2[dado] === 'Sim'){
-      console.log('Ambos recorrentes');
-    }
-    else {
-      console.log(d1[dado] + ' e ' + d2[dado]);
-    }
-  }
-}
-imprimeAmbos (info, info2);
+console.log(
+  "O livro favorito de " +
+    leitor.nome +
+    " " +
+    leitor.sobrenome +
+    " se chama " +
+    infoBook
+);
